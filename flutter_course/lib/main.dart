9ec20e0cbedc9206561c96f115dc409befc43103
martinmutuku <martinnzioka@gmail.com>
2019-06-12 18:20:38 +0300

    The stateful app

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
-main(){
 
+main()=>  runApp(MyApp());
+
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    //TODO: implement createState
+    return _MyAppState();
+  }
 }
-class MyApp extends StatelessWidget{
+class _MyAppState extends State<MyApp> {
+  List<String> _Products = ['Food Tester'];
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp(home:Scaffold(appBar:,),);
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('EasyList'),
+          ),
+          body: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text('Add Product'),
+                  ),),
+                Column(
+                  children: _Products
+                      .map((element) =>
+                      Card(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset('assets/foods.jpg'),
+                            Text(element)
+                          ],
+                        ),
+                      ))
+                      .toList(),
+                ),
+              ])),
+    );
   }
 }
